cc7bd79db535c39ad1c180c049b12cfa09268748
Matt Payne <Payne@MattPayne.org>
2018-08-04 09:13:05 -0500

    Fix SafariBooksOnline.com

---
+++ sb.js
@@ -0,0 +1,6 @@
+// from the https://www.youtube.com/watch?v=DloHqUfPbJc
+(function() {
+  let clazz="src-CardSlider-cardSlider";
+  let cusid_ele = document.getElementsByClassName(clazz);
+  cusid_ele[0].hidden=true
+})();
